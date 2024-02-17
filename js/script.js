9074c012236dd78ad99ba338ee51a22a56ea020c
Wiliam Gonçalves Souza <63932089+WiliamGSouza@users.noreply.github.com>
2024-02-17 15:39:45 -0300

    Perguntas de futebol

--- js/script.js
+++ js/script.js
@@ -1,95 +1,96 @@
 const perguntas = [
     {
-        pergunta: 'Qual é a função usada para imprimir algo no console?',
+        pergunta: 'Qual clube brasileiro possui o maior número de títulos da Copa Libertadores da América?',
         respostas: [
-            "console.log()",
-            "print()",
-            "display()"
+            "Santos FC",
+            "SE Palmeiras",
+            "CR Flamengo"
         ],
         correta: 0
     },
     {
-        pergunta: 'Qual declaração de variável é utilizada para criar uma variável que pode ser modificada posteriormente?',
+        pergunta: 'Quem é o maior artilheiro da história da Seleção Brasileira de Futebol?',
         respostas: [
-            "const",
-            "let",
-            "var"
+            "Pelé",
+            "Ronaldo",
+            "Romário"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: 'Qual é o único clube brasileiro a ter conquistado a Tríplice Coroa (Campeonato Brasileiro, Copa do Brasil e Copa Libertadores no mesmo ano)?',
+        respostas: [
+            "Grêmio FBPA",
+            "São Paulo FC",
+            "CR Flamengo"
+        ],
+        correta: 2
+    },
+    {
+        pergunta: 'Quantas vezes o Brasil ganhou a Copa do Mundo de Futebol?',
+        respostas: [
+            "5 vezes",
+            "6 vezes",
+            "7 vezes"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: 'Qual jogador brasileiro é conhecido como "O Fenômeno"?',
+        respostas: [
+            "Ronaldinho Gaúcho",
+            "Ronaldo Nazário",
+            "Neymar Jr."
         ],
         correta: 1
     },
     {
-        pergunta: 'O que o operador === faz em JavaScript?',
+        pergunta: 'Qual é o estádio de futebol mais famoso do Brasil?',
         respostas: [
-            "Verifica se os valores são iguais, mas ignora o tipo",
-            "Verifica se os valores e os tipos são iguais",
-            "Verifica se os valores são diferentes"
+            "Estádio do Maracanã",
+            "Arena Corinthians",
+            "Estádio Mineirão"
+        ],
+        correta: 0
+    },
+    {
+        pergunta: 'Qual clube é conhecido como "O Time de Guerreiros"?',
+        respostas: [
+            "SC Corinthians Paulista",
+            "Fluminense FC",
+            "Grêmio FBPA"
         ],
         correta: 1
     },
     {
-        pergunta: 'Qual é a função usada para converter uma string em um número inteiro em JavaScript?',
+        pergunta: 'Qual foi o primeiro clube brasileiro a conquistar a Copa Libertadores da América?',
         respostas: [
-            "parseInt()",
-            "parseFloat()",
-            "toInteger()"
+            "Santos FC",
+            "CR Flamengo",
+            "SE Palmeiras"
         ],
         correta: 0
     },
     {
-        pergunta: 'Como se chama o conceito em JavaScript que permite que uma função tenha acesso às variáveis de sua função externa?',
+        pergunta: 'Quem é o jogador brasileiro com mais gols marcados na história da Liga dos Campeões da UEFA?',
         respostas: [
-            "Injeção de dependência",
-            "Escopo léxico",
-            "Escopo dinâmico"
+            "Ronaldo Nazário",
+            "Neymar Jr.",
+            "Rivaldo"
         ],
         correta: 1
     },
     {
-        pergunta: 'Qual método é usado para remover o último elemento de um array em JavaScript?',
+        pergunta: 'Qual foi o único clube brasileiro a vencer a Copa Intercontinental (Mundial Interclubes) por três vezes consecutivas?',
         respostas: [
-            "pop()",
-            "remove()",
-            "delete()"
-        ],
-        correta: 0
-    },
-    {
-        pergunta: 'O que o operador && faz em JavaScript?',
-        respostas: [
-            "Verifica se pelo menos uma das expressões é verdadeira",
-            "Verifica se ambas as expressões são verdadeiras",
-            "Verifica se pelo menos uma das expressões é falsa"
-        ],
-        correta: 1
-    },
-    {
-        pergunta: 'Como se chama o tipo de dado que pode representar verdadeiro ou falso?',
-        respostas: [
-            "Boolean",
-            "String",
-            "Number"
-        ],
-        correta: 0
-    },
-    {
-        pergunta: 'Qual é a função usada para adicionar um elemento ao final de um array em JavaScript?',
-        respostas: [
-            "push()",
-            "append()",
-            "addToEnd()"
-        ],
-        correta: 0
-    },
-    {
-        pergunta: 'O que o operador || faz em JavaScript?',
-        respostas: [
-            "Verifica se pelo menos uma das expressões é verdadeira",
-            "Verifica se ambas as expressões são verdadeiras",
-            "Verifica se pelo menos uma das expressões é falsa"
+            "São Paulo FC",
+            "CR Flamengo",
+            "SE Palmeiras"
         ],
         correta: 0
     }
 ];
+
 
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector("template")
